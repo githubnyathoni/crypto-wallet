033810acfdbefd2bd75f780611b3ad9473838d5d
Ahmad Fathoni <ahmad.fathoni50@gmail.com>
2024-10-17 16:20:19 +0700

    ❌ fix validation top up less than 10million

--- src/application/dtos/topup.dto.ts
+++ src/application/dtos/topup.dto.ts
@@ -3,6 +3,6 @@ import { IsInt, IsPositive, Max } from 'class-validator';
 export class TopUpDto {
   @IsInt()
   @IsPositive()
-  @Max(10000000, { message: 'Top-up amount must be less than 10.000.000' })
+  @Max(9999999, { message: 'Top-up amount must be less than 10.000.000' })
   amount: number;
 }
