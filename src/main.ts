1069f2bdd0f37db8a19a7e219d1e56eaa69ad96a
Ahmad Fathoni <ahmad.fathoni50@gmail.com>
2024-10-19 20:34:07 +0700

    ❌ fix deploying vercel and rollback port to 3000

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+let app;
+
 export const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+  if (!app) {
+    const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('/v1/api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-  app.enableCors();
+    app.setGlobalPrefix('/v1/api');
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    app.enableCors();
 
-  await app.listen(8080);
+    await app.listen(3000);
+  }
 
   return app;
 };
